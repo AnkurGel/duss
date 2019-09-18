8284709f06f61f2b7dff9705fa64e09461279d81
Ankur Goel <ankurgel@gmail.com>
2019-09-18 13:31:08 +0530

    build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd $GOPATH/src/github.com/ankurgel/duss
+echo "Installing build dependencies..."
+go get -v github.com/mitchellh/gox
+go get -v github.com/gobuffalo/packr/v2/packr2
+echo "Installing dependencies..."
+dep ensure
+echo "Packing build..."
+cd ./cmd/duss && packr2 clean && packr2 && cd ../../
+rm -rf releases
+mkdir releases
+echo "Preparing binaries..."
+gox -output="releases/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="linux/amd64 darwin/amd64" ./cmd/duss
+echo "Cleaning up..."
+cd ./cmd/duss && packr2 clean && cd ../../
+echo "Binaries ready in ./releases/"
