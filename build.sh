ed88c2db1e73351649504be45e2a537cb4f75c9d
Akhil Lb <akhil.lb1@gmail.com>
2019-09-23 14:57:00 +0530

    Release build support with no symbols

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 export PATH="$PATH:$GOBIN"
+
+export BUILD_TYPE=${1:-"debug"}
+export LDFLAGS=''
+if [[ $BUILD_TYPE == 'release' ]]; then
+  export LDFLAGS='-s -w'
+fi
+echo "Building for $BUILD_TYPE"
+
 cd $GOPATH/src/github.com/ankurgel/duss
 echo "Installing build dependencies..."
 go get -v github.com/mitchellh/gox
@@ -11,7 +19,7 @@ cd ./cmd/duss && packr2 clean && packr2
 rm -rf ../../releases
 mkdir ../../releases
 echo "Preparing binaries..."
-gox -output="../../releases/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="linux/amd64 darwin/amd64"
+gox -output="../../releases/{{.Dir}}_{{.OS}}_{{.Arch}}" -osarch="linux/amd64 darwin/amd64" -ldflags="$LDFLAGS"
 echo "Cleaning up..."
 packr2 clean
 cd ../../
